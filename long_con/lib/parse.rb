066b14bbba2d1f7d450e8ed5376d0fb20ad0bb38
JinLee <jinlee0908@gmail.com>
2014-03-12 21:19:06 -0700

    added benchmark app and revised spec files

--- long_con/lib/parse.rb
+++ long_con/lib/parse.rb
@@ -1,18 +1,17 @@
 # this module is parsing form data for the suckers
-module Parse
-
+class Parse
   def parse_stuff(name, phone, twitter, email)
     @prefixes = ['Mrs.', 'Miss', 'Ms', 'Dr.', 'Mr.', 'mrs.', 'miss', 'ms', 'dr.', 'mr.']
     @suffixes = ['DDS', 'MD', 'Sr.', 'IV', 'DVM', 'I', 'II', 'Jr.', 'V', 'III', 'Phd']
     csv_ready = []
-    csv_ready << Parse::parse_names(@prefixes, @suffixes, name)
-    csv_ready << Parse::parse_numbers(phone)
-    csv_ready << Parse::parse_twitter(twitter)
-    csv_ready << Parse::parse_email(email)
+    csv_ready << Parse.parse_names(@prefixes, @suffixes, name)
+    csv_ready << Parse.parse_numbers(phone)
+    csv_ready << Parse.parse_twitter(twitter)
+    csv_ready << Parse.parse_email(email)
     csv_ready
   end
 
-  def parse_names(prefixes, suffixes, name_string)
+  def self.parse_names(prefixes, suffixes, name_string)
     parsed_name = { pre: '', first: '', middle: '', last: '', suffix: '' }
     word = name_string.split
     parsed_name[:suffix] = word.pop if suffixes.include? word.last
@@ -23,17 +22,17 @@ module Parse
     parsed_name.values
   end
 
-  def parse_twitter(data)
+  def self.parse_twitter(data)
     twitter = /\w+/
     [twitter.match(data).to_s]
   end
 
-  def parse_email(email_string)
+  def self.parse_email(email_string)
     email_regex = /\w+\@\w+\.\w+/
     email_regex.match(email_string) ? [email_string] : ['Not Found']
   end
 
-  def parse_numbers(numbers)
+  def self.parse_numbers(numbers)
     parse_number = {  country: '', area: '', prefix: '', line: '', ext: '' }
     num = numbers.scan(/\d+/)
     parse_number[:country] = num.shift if num[0] == '1'
@@ -43,5 +42,4 @@ module Parse
     parse_number[:ext] = num.shift || num[0] = ''
     parse_number.values
   end
-
 end
