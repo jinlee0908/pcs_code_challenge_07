6f1b8fc083784e545a0459b1ecd0918fd3f62e80
hollocherDale <hollocher@gmail.com>
2014-03-13 18:59:25 -0700

    begun working on model

--- long_con/long_con_app.rb
+++ long_con/long_con_app.rb
@@ -1,13 +1,37 @@
 require 'sinatra'
-require 'csv'
+require 'data_mapper'
+
 require_relative 'lib/parse.rb'
 require_relative 'lib/process_csv.rb'
 enable :sessions
 
+DataMapper.setup(:default, "sqlite3://#{settings.root}/suckers20.sqlite3")
+
+class Sucker
+  include DataMapper::Resource
+  property :id, Serial
+  property :prefix, String
+  property :first_name, String
+  property :middle_name, String
+  property :last_name, String
+  property :suffix, String
+  property :country_code, String
+  property :area_code, String
+  property :prefix, String
+  property :line, String
+  property :extension, String
+  property :twitter, String
+  property :email, String
+  property :created_at, DateTime
+end
+
+DataMapper.finalize
+Sucker.auto_upgrade!
+
 before do
   @heading = 'Born Every Minute'
   @foot = 'Copyright &copy 2014 The Long Con LLC'
-  @data_path = "#{settings.public_folder}/people.csv"
+  @data_path = "#{settings.public_folder}/suckers20.sqlite3"
 end
 
 get '/' do
@@ -18,9 +42,10 @@ post '/suckers' do
   if params.empty?
     redirect '/'
   else
-    @new_sucker = Parse.new.parse_stuff(params[:name], params[:phone], params[:twitter], params[:email])
-    ProcessCSV.new.write_to_csv(@data_path, @new_sucker)
-    session[:name] = params[:name]
+    @new_sucker = Sucker.new(params[:sucker])
+    #or
+    Sucker.create params[:sucker]
+    session[:name] = params[:sucker].name
     redirect '/thanks'
   end
 end
@@ -31,12 +56,13 @@ get '/thanks' do
 end
 
 get '/suckers' do
-  # "Dir is: #{settings.public_folder}"
-  @people = ProcessCSV.new.fetch_suckers_data(@data_path)
+  # "display all the suckers"
+  @people = ??????
   erb:suckers
 end
 
 get '/suckers/:id' do
-  @people = ProcessCSV.new.fetch_single_sucker(@data_path, params[:id])
+  # display an individual sucker
+  @people = Suckers.get params[:id]
   erb:suckers_specific
 end
