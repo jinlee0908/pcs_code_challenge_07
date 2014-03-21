f15852ca7ed3dbfec1216bc2389428e5e92ba3e7
hollocherDale <hollocher@gmail.com>
2014-03-20 17:17:02 -0700

    removed unused test file

--- long_con/spec/long_con_spec.rb
+++
@@ -1,8 +0,0 @@
-before do
-  @test_data = { name: 'Mrs. Theresa E. Stamm',
-                 email: 'kieran@runte.biz',
-                 phone: '1-678-523-6736',
-                 twitter: '@Reinger' }
-
-#  @sucker = Sucker.new(@test_data)
-end
